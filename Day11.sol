6c433e85e8990c9d90030ab72dd7258caf9bb7da
16sixteen16 <sjycjf0816@gmail.com>
2026-03-15 15:54:03 +0800

    Refactor VaultMaster contract and remove ShiliuSale

--- Day11.sol
+++ Day11.sol
@@ -1,43 +1,39 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract ShiliuToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("Shiliu Token", "SLT") {
-        _mint(msg.sender, initialSupply * 10 ** decimals());
-    }
-}
+contract VaultMaster is Ownable {
+    mapping(address => uint256) public balances;
 
-contract ShiliuSale is Ownable {
-    ShiliuToken public tokenContract;
-    uint256 public tokenPrice = 1000;
+    event Deposit(address indexed user, uint256 amount);
+    event Withdraw(address indexed user, uint256 amount);
 
-    event Sold(address indexed buyer, uint256 amount);
+    constructor() Ownable(msg.sender) {}
 
-    constructor(address _tokenAddress) Ownable(msg.sender) {
-        tokenContract = ShiliuToken(_tokenAddress);
+    function deposit() public payable {
+        require(msg.value > 0, "Amount must be greater than 0");
+        balances[msg.sender] += msg.value;
+        emit Deposit(msg.sender, msg.value);
     }
 
-    function buyTokens() public payable {
-        uint256 _amount = msg.value * tokenPrice;
-        require(tokenContract.balanceOf(address(this)) >= _amount, "Insufficient tokens in contract");
-        require(tokenContract.transfer(msg.sender, _amount), "Transfer failed");
-        emit Sold(msg.sender, _amount);
+    function withdraw(uint256 _amount) public {
+        require(balances[msg.sender] >= _amount, "Insufficient balance");
+        
+        balances[msg.sender] -= _amount;
+        (bool success, ) = payable(msg.sender).call{value: _amount}("");
+        require(success, "Transfer failed");
+        
+        emit Withdraw(msg.sender, _amount);
     }
 
-    receive() external payable {
-        buyTokens();
+    function getVaultBalance() public view onlyOwner returns (uint256) {
+        return address(this).balance;
     }
 
-    function withdraw() public onlyOwner {
-        uint256 balance = address(this).balance;
-        (bool success, ) = payable(owner()).call{value: balance}("");
-        require(success, "Withdraw failed");
-    }
-
-    function tokensAvailable() public view returns (uint256) {
-        return tokenContract.balanceOf(address(this));
+    function emergencyWithdraw() public onlyOwner {
+        uint256 amount = address(this).balance;
+        (bool success, ) = payable(owner()).call{value: amount}("");
+        require(success, "Emergency withdraw failed");
     }
 }
